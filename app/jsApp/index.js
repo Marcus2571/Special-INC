6843d2242e09f90a807a9425804cf2b712f9ebf3
Mathias Eriksen <mattyopyt@hotmail.com>
2023-08-18 09:01:08 +0200

    Alt sammensat

--- app/jsApp/index.js
+++ app/jsApp/index.js
@@ -59,5 +59,27 @@ Object.keys(lcidDictionary).forEach((key) => {
     Translate.appendChild(clone);
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+    let video = document.getElementById("vid");
+    let mediaDevices = navigator.mediaDevices;
+    vid.muted = true;
+
+    // Accessing the user camera and video.
+    mediaDevices
+        .getUserMedia({
+            video: true,
+            audio: true,
+        })
+        .then((stream) => {
+
+            // Changing the source of video to current stream.
+            video.srcObject = stream;
+            video.addEventListener("loadedmetadata", () => {
+                video.play();
+            });
+        })
+        .catch(alert);
+});
+
 // document.appendChild(Spoken);
 // document.appendChild(Translate);
